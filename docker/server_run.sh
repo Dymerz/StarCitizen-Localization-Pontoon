9bec7b61aa15eb6b1f54f7461b57e4d302ca47b3
Dymerz <21324342+Dymerz@users.noreply.github.com>
2025-05-13 02:40:34 +0200

    Initialize Pontoon by duplicating default locales in server_run.sh

--- docker/server_run.sh
+++ docker/server_run.sh
@@ -5,5 +5,26 @@
 echo ">>> Setting up the db for Django"
 python manage.py migrate
 
+echo ">>> Initializing Pontoon"
+echo "from pontoon.base.models import Locale;
+def duplicate_locale(original_code, name):
+  # Check if locale with the new name already exists
+  if Locale.objects.filter(code=name).exists():
+    return
+
+  # Get the original locale and duplicate it
+  original = Locale.objects.get(code=original_code)
+  duplicate = Locale.objects.get(pk=original.pk)
+  duplicate.code = name
+  duplicate.save()
+
+duplicate_locale('en', 'english');
+duplicate_locale('fr', 'french_(france)');
+duplicate_locale('de', 'german_(germany)');
+duplicate_locale('it', 'italian_(italy)');
+duplicate_locale('pt-BR', 'portuguese_(brazil)');
+duplicate_locale('es-ES', 'spanish_(spain)');
+" | python manage.py shell
+
 echo ">>> Starting local server"
 exec python manage.py runserver 0.0.0.0:8000
